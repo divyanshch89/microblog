8a1c9a8e09e48c3ba6ded79d90ffe3c73850e488
Divyansh Chaudhary <divyanshch@outlook.com>
2021-01-27 16:15:06 -0600

    added support for user profile and the custom errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 # flash is the module and the Flask is the class
 
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 globalapp = Flask(__name__)
 globalapp.config.from_object(Config)
@@ -14,4 +19,35 @@ login = LoginManager(globalapp)
 login.login_view = 'login'
 migrate = Migrate(globalapp, db)
 
-from app import routes, models
+if not globalapp.debug:
+    if globalapp.config['MAIL_SERVER']:
+        auth = None
+        if globalapp.config['MAIL_USERNAME'] or globalapp.config['MAIL_PASSWORD']:
+            auth = (globalapp.config['MAIL_USERNAME'], globalapp.config['MAIL_PASSWORD'])
+        secure = None
+        if globalapp.config['MAIL_USE_TLS']:
+            globalapp = ()
+        mail_handler = SMTPHandler(
+            mailhost=(globalapp.config['MAIL_SERVER'], globalapp.config['MAIL_PORT']),
+            fromaddr='no-reply@' + globalapp.config['MAIL_SERVER'],
+            toaddrs=globalapp.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        globalapp.logger.addHandler(mail_handler)
+
+if not globalapp.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    globalapp.logger.addHandler(file_handler)
+
+    globalapp.logger.setLevel(logging.INFO)
+    globalapp.logger.info('Microblog startup')
+
+from app import routes, models, errors
